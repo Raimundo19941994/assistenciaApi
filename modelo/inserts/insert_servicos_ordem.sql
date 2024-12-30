73d38dbd3d39fb2fd7d0bb01cd33b5d00ea5226a
R1994 <92157561+Raimundo19941994@users.noreply.github.com>
2024-12-29 22:30:18 -0300

    Delete modelo directory

--- modelo/inserts/insert_servicos_ordem.sql
+++
@@ -1,7 +0,0 @@
-select * from servicos_ordem;
-insert into servicos_ordem(quantidade, preco, servicos_id, ordem_id)
-values  ('1','300','1','1'),
-		('1','400','2','2'),
-        ('1','150','3','3'),
-        ('1','100','4','4'),
-        ('1','100','5','5');
